2e7c7b6afc890f04b321590b98e8dff2f8a0edb7
Aryan Sinha <aryananusinha@gmail.com>
2025-05-22 16:38:57 -0400

    Added barbar.nvim

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        "romgrk/barbar.nvim",
+        dependencies = {
+            "lewis6991/gitsigns.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            
+        },
+        config = function()
+            vim.g.barbar_auto_setup = false
+
+            local map = vim.api.nvim_set_keymap
+            local opts = { noremap = true, silent = true }
+
+            map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+            map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+            map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+            map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+        end,
+        version = '^1.0.0',
+    },
+}
+
