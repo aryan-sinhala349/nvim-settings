5fab96985c3d4152d8a25a7b6e76343128138a98
Aryan Sinha <aryananusinha@gmail.com>
2025-05-24 09:11:58 -0400

    Added toggle between absolute and hybrid line numbers, and opened Neotree at startup

--- init.lua
+++ init.lua
@@ -4,5 +4,9 @@ vim.cmd.set("expandtab")
 vim.cmd.set("smarttab")
 vim.cmd.set("shiftwidth=4")
 
+vim.keymap.set('n', 'm', function() vim.cmd.set("rnu!") end, {})
+
 require("config.lazy")
+
+vim.cmd("Neotree<CR>")
 
